0ec05b6c229fe6155904754ba78cdd5dbba44ef1
vdadfar <vdadfar@yext.com>
2021-08-17 13:44:52 -0400

    messing around

--- src/index.js
+++ src/index.js
@@ -1,9 +1,21 @@
+import GoTrue from 'gotrue-js';
+
 import './global.css';
 
 function importAll(r) {
-    let images = {};
-    r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
-    return images;
-  }
-  
-  const images = importAll(require.context('./images', false, /\.(png|jpe?g|svg)$/));
+  let images = {};
+  r.keys().map((item, index) => { images[item.replace('./', '')] = r(item); });
+  return images;
+}
+
+const images = importAll(require.context('./images', false, /\.(png|jpe?g|svg)$/));
+
+function pageLoad() {
+  // Instantiate the GoTrue auth client with an optional configuration
+}
+
+if (document.readyState === "complete" || document.readyState === "loaded") {
+  pageLoad();
+}
+
+document.addEventListener("DOMContentLoaded", pageLoad(), false);
